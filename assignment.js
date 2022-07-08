a8f6141220a43a78b78af486fc956e14c0cd2665
Denine Guy <denineguy@Denines-MacBook-Pro.local>
2022-07-08 09:37:38 -0400

    updated query element

--- assignment.js
+++ assignment.js
@@ -1,19 +1,19 @@
 //Question 1:
 //Select this task (in two different ways at least!) and change the background-color to black, text-color to white.
 
-const task = document.querySelector('#task-1');
+const task = document.querySelector('li');
 task.style.color = "white"
 task.style.backgroundColor = "black"
 
 
-const listItem = document.querySelector('li');
+const listItem = document.getElementById('#task-1');
 listItem.style.color = "white"
 listItem.style.backgroundColor = "black"
 
 // Question 2:
 // Change the document title (in <head></head>) to "Assignment - Solved!". Use two different ways for getting access to the <title> element: Via querySelector on document and via querySelector on the certain property you find in document.
-// const title1 = document.querySelector("title")
-// title1.innerText = "Assignment - Solved!"
+const title1 = document.querySelector("title")
+title1.innerText = "Assignment - Solved!"
 
 let title2 = document.head.querySelector("title")
 title2.innerText = "Assignment - Solved"
